9fdc8bb73cbfa1acf1e2e42e756d87d64f9d1a3c
shirocodes <ishirapt@gmail.com>
2025-04-20 09:53:27 +0300

    lift state: added shared state for stock component and portfolio update.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [portfolio, setPortfolio] = useState([])
+
+  function handleBuyStockClick(addedStock) {
+    setPortfolio([...portfolio, addedStock])
+
+  }
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer onBuyStock={handleBuyStockClick} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} />
         </div>
       </div>
     </div>
